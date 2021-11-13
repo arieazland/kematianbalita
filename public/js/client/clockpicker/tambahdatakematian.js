46503d8e61bd6af8c491878a289ee801ed590443
Firly Arie Azland <arieazland@gmail.com>
2021-11-13 09:23:08 +0700

    Pertanyaan Nomor 3

---
+++ public/js/client/clockpicker/tambahdatakematian.js
@@ -0,0 +1,19 @@
+$('#single-input').clockpicker({
+    placement: 'bottom',
+    align: 'left',
+    autoclose: true,
+    'default': 'now'
+});
+$('.clockpicker').clockpicker({
+    donetext: 'Done',
+}).find('input').change(function() {
+    console.log(this.value);
+});
+$('#check-minutes').click(function(e) {
+    // Have to stop propagation here
+    e.stopPropagation();
+    input.clockpicker('show').clockpicker('toggleView', 'minutes');
+});
+if (/mobile/i.test(navigator.userAgent)) {
+    $('input').prop('readOnly', true);
+}
